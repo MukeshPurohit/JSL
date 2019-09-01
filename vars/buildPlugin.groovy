8e1ab28fbe8c513daef7dcc5902a16359cc8bc65
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-01 12:15:07 +0530

    Add Initial files

---
+++ vars/buildPlugin.groovy
@@ -0,0 +1,83 @@
+#!/usr/bin/env groovy
+
+/**
+ * Simple wrapper step for building a plugin
+ */
+def call(List<Integer> jdkVersions = [7, 8],
+        String repo = null,
+        Boolean failFast = true,
+        List<String> platforms = ['linux', 'windows']) {
+    Map tasks = [:]
+
+    for (int i = 0; i < platforms.size(); ++i) {
+        for (int j = 0; j < jdkVersions.size(); ++j) {
+            String label = platforms[i]
+            String jdk = jdkVersions[j]
+            String stageIdentifier = "${label}-${jdk}"
+
+            tasks["${label}-${jdk}"] = {
+                node(label) {
+                    stage("Checkout (${stageIdentifier})") {
+                        if (env.BRANCH_NAME) {
+                            timestamps {
+                                checkout scm
+                            }
+                        }
+                        else if ((env.BRANCH_NAME == null) && (repo)) {
+                            timestamps {
+                                git repo
+                            }
+                        }
+                        else {
+                            error 'buildPlugin must be used as part of a Multibranch Pipeline *or* a `repo` argument must be provided'
+                        }
+                    }
+
+                    stage("Build (${stageIdentifier})") {
+                        List<String> mavenOptions = [
+                            '--batch-mode',
+                            '--errors',
+                            '--update-snapshots',
+                            '-Dmaven.test.failure.ignore=true',
+                            "-DskipAfterFailureCount=${failFast}",
+                        ]
+                        String mavenCommand = "mvn ${mavenOptions.join(' ')} clean install"
+                        String jdkTool = "jdk${jdk}"
+
+                        withEnv([
+                            "JAVA_HOME=${tool jdkTool}",
+                            "PATH+MAVEN=${tool 'mvn'}/bin",
+                            'PATH+JAVA=${JAVA_HOME}/bin',
+                        ]) {
+                            if (isUnix()) {
+                                timestamps {
+                                    sh mavenCommand
+                                }
+                            }
+                            else {
+                                timestamps {
+                                    bat mavenCommand
+                                }
+                            }
+                        }
+                    }
+
+                    stage("Archive (${stageIdentifier})") {
+                        timestamps {
+                            junit '**/target/surefire-reports/**/*.xml'
+                            archiveArtifacts artifacts: '**/target/*.hpi,**/target/*.jpi',
+                                        fingerprint: true
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /* If we cannot complete in 60 minutes, we should fail the build. Compute
+     * isn't free!
+     */
+    timeout(60) {
+        return parallel(tasks)
+    }
+}
