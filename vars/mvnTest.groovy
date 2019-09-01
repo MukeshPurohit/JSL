843cffa1d43f7ff75f2411a4a06f31f51bd3ea28
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-01 12:52:13 +0530

    Update mvnTest.groovy

--- vars/mvnTest.groovy
+++ vars/mvnTest.groovy
@@ -1,11 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String mavenName = 'M3'){
-    stage('Tests'){
-        withMaven(
-            maven: "${mavenName}"
-        ){
-            sh "mvn test"
-        }
-    }
+def call(String mavenToUse = 'M3'){
+  // using the Pipeline Maven plugin we can set maven configuration settings, publish test results, and annotate the Jenkins console
+  withMaven(
+    maven: "${mavenToUse}"
+  ){
+      sh 'mvn clean test'
+  }
 }
