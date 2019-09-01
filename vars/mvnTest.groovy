17aebd7965f52a0b4c279ab8410b941cb87da1cb
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-01 16:38:04 +0530

    Update mvnTest.groovy

--- vars/mvnTest.groovy
+++ vars/mvnTest.groovy
@@ -6,31 +6,4 @@ def call(String mavenName = 'M3'){
             sh "mvn test"
         }
     }
-    stage('Sonar'){
-        withMaven(
-            maven: "${mavenName}"
-        ){
-            script{
-                try{
-                  sh "mvn sonar:sonar"
-                }catch (err){
-                    echo 'Ate the error in sonar' //Temporarily
-                }
-            }
-        }
-    }
-    stage('Package'){
-        withMaven(
-            maven: "${mavenName}"
-        ){
-            sh "mvn clean install -DskipTests -DskipITs"
-        }
-    }
-    stage('Push to Nexus'){
-        withMaven(
-            maven: "${mavenName}"
-        ){
-            sh "mvn deploy -DskipTests -DskipITs"
-        }
-    }
 }
