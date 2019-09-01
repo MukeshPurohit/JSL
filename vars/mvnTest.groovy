a9042f1294f07fcfc018e3ef5d211f4ab6fa3312
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-01 12:54:05 +0530

    Update mvnTest.groovy

--- vars/mvnTest.groovy
+++ vars/mvnTest.groovy
@@ -2,9 +2,5 @@
 
 def call(String mavenToUse = 'M3'){
   // using the Pipeline Maven plugin we can set maven configuration settings, publish test results, and annotate the Jenkins console
-  withMaven(
-    maven: "${mavenToUse}"
-  ){
       sh 'mvn clean test'
-  }
-}
+ }
