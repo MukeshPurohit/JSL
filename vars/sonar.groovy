b34597e1273802440a3709b043eb62b79410abfa
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-01 13:10:33 +0530

    Initial Sonar File

---
+++ vars/sonar.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+
+def call(String mavenName = 'M3'){
+    stage('Sonar'){
+        withMaven(
+            maven: "${mavenName}"
+        ){
+            script{
+                try{
+                  sh "mvn sonar:sonar"
+                }catch (err){
+                    echo 'Ate the error in sonar' //Temporarily
+                }
+            }
+        }
+    }
+}
