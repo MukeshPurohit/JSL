34535b4fb7b35ef70aac8e2fede99cdc23505c6b
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-01 19:26:18 +0530

    Update sonarGates.groovy

--- vars/sonarGates.groovy
+++ vars/sonarGates.groovy
@@ -1,13 +1,24 @@
-stage("Quality Gate"){
-           steps {
-                script {
-                
-                    timeout(time: 160, unit: 'SECONDS') {
-                        def qg = waitForQualityGate()
-                        if (qg.status != 'OK') {
-                            error "Pipeline aborted due to quality gate failure: ${qg.status}"
-                        }
-                    }
+#!/usr/bin/env groovy
+
+def call(String mavenName = 'M3'){
+    stage('Quality Gate'){
+        withMaven(
+            maven: "${mavenName}"
+        ){
+            script{
+                try{
+                    withSonarQubeEnv('sonar') {
+                                 timeout(time: 160, unit: 'SECONDS') {
+                                            def qg = waitForQualityGate()
+                                            if (qg.status != 'OK') {
+                                                       error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                                                      }
+                                                   }
+                                              }
+                }catch (err){
+                               echo 'Error in sonar processing. Quality gate failure...'
+                           }
                 }
-            }
         }
+    }
+}
