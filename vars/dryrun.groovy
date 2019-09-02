73ec754fbb2065bdec4cba9416dbd2cfef291639
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-02 09:49:01 +0530

    Update dryrun.groovy

--- vars/dryrun.groovy
+++ vars/dryrun.groovy
@@ -1,4 +1,21 @@
+def call ( body ) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    try {
+        body()
+    } catch(e) {
+        currentBuild.result = "FAILURE";
+        throw e;
+    } finally {
+
+        config.each{ k, v -> println "${k}:${v}" }
+
+    }
+}
+/*
 def test(String type, String parallel = 'yes') {
     println(type)
     println(parallel)
 }
+*/
