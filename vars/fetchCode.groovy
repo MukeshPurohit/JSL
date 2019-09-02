b60a87a8cd804610b7b966d8e2cd1621b3a45ad0
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-02 14:38:10 +0530

    Update fetchCode.groovy

--- vars/fetchCode.groovy
+++ vars/fetchCode.groovy
@@ -1,26 +1,19 @@
 #!/usr/bin/env groovy
 import com.bt.java.BuildConfig
-
 def call(def body = [:]) {
     echo "calling resolve method of class"
     config = BuildConfig.resolve(body)
-    config.each{ k, v -> println "${k} <--> ${v}" }
-    echo "***inside fetchCode.groovy**************"config.GitURL
-    }
-/*
-def call(String mavenName = 'M3'){
-    stage('Checkout'){
+    stage('Checkout Git repo'){
         withMaven(
             maven: "${mavenName}"
         ){
             script{
                 try{
-                  git url: 'https://github.com/jglick/simple-maven-project-with-tests.git'
+                      git url: "${config.GitURL}"
                 }catch (err){
-                    echo 'Error while Repo fetch'
+                    echo 'Error while fetching maven repo ' "${config.GitURL}"
                 }
             }
         }
     }
 }
-*/
