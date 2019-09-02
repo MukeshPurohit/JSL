619ad69c734c0dec7b160abddbfcec6dc508c6d8
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-02 17:03:35 +0530

    Update fetchCode.groovy

--- vars/fetchCode.groovy
+++ vars/fetchCode.groovy
@@ -9,8 +9,16 @@ def call(def body = [:]) {
             script{
                 try{
                       git url: "${config.GitURL}"
-                }catch (err){
+                }catch (err)
+                {
                     echo 'Error while retrieving maven repo ' "${config.GitURL}"
+/*                  String recipient = 'deg@bt.com'
+                    mail subject: "${env.JOB_NAME} (${env.BUILD_NUMBER}) failed",
+                    body: "It appears that ${env.BUILD_URL} is failing for code ${config.GitURL} ",
+                    to: recipient,
+                    replyTo: recipient,
+                    from: 'noreply@bt.com'
+*/
                 }
             }
         }
