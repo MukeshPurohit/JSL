6e990464801d8ea0abc1fe854912595745b85a65
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-02 15:10:33 +0530

    Update sonarCQ.groovy

--- vars/sonarCQ.groovy
+++ vars/sonarCQ.groovy
@@ -1,16 +1,22 @@
 #!/usr/bin/env groovy
 import com.bt.java.BuildConfig
-def call(def body = [:]) {
+def call(def body = [:]) 
+{
     config = BuildConfig.resolve(body)
-    stage('Sonar'){
-        withMaven(
-            maven: "${mavenName}"
-        ){
-            script{
-                try{
-                    withSonarQubeEnv('sonar') {
+    stage('Sonar')
+    {
+        withMaven(maven: "${mavenName}")
+        {
+            script
+            {
+                try
+                {
+                    withSonarQubeEnv('sonar') 
+                    {
                         sh "mvn sonar:sonar"
-                }catch (err){
+                    }
+                }catch (err)
+                {
                     echo 'Error in sonar processing' "${config.SonarURL}" 'on REPO ' "${config.GitURL}"
                 }
             }
