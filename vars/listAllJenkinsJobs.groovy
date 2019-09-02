5758c40e04baf3f7c2433c8ac80d5123b79e1199
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-02 16:18:14 +0530

    Update listAllJenkinsJobs.groovy

--- vars/listAllJenkinsJobs.groovy
+++ vars/listAllJenkinsJobs.groovy
@@ -1,5 +1,29 @@
+#!/usr/bin/env groovy
+import com.bt.java.BuildConfig
 import hudson.model.*
 
-hudson.model.Hudson.instance.items.findAll{job -> job}.each {
+
+def call(def body = [:]) 
+{
+    config = BuildConfig.resolve(body)
+    hudson.model.Hudson.instance.items.findAll{job -> job}.each 
+    {
     job -> println("Job: " + job.name)
+    }
 }
+/*
+    stage('Checkout Git repo'){
+        withMaven(
+            maven: "${mavenName}"
+        ){
+            script{
+                try{
+                      git url: "${config.GitURL}"
+                }catch (err){
+                    echo 'Error while retrieving maven repo ' "${config.GitURL}"
+                }
+            }
+        }
+    }
+}
+*/
