2b7a4ffe9d0d23c8c824b3999ca1b14fc6d2f93d
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-02 13:45:08 +0530

    Update loadBody.groovy

--- vars/loadBody.groovy
+++ vars/loadBody.groovy
@@ -3,4 +3,5 @@ import com.bt.java.BuildConfig
 def call(def body = [:]) {
     echo "calling resolve method of class"
     config = BuildConfig.resolve(body)
+    config.each{ k, v -> println "${k} <--> ${v}" }
     }
