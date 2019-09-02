dff58f8c8a03a9a5d7d6c864003130f909486013
MukeshPurohit <purohit.mukesh@gmail.com>
2019-09-02 14:50:08 +0530

    Update mvnTest.groovy

--- vars/mvnTest.groovy
+++ vars/mvnTest.groovy
@@ -1,9 +1,29 @@
+/*
 def call(String mavenName = 'M3'){
     stage('Tests'){
         withMaven(
             maven: "${mavenName}"
         ){
             sh "mvn test"
+        }
+    }
+}
+*/
+#!/usr/bin/env groovy
+import com.bt.java.BuildConfig
+def call(def body = [:]) {
+    config = BuildConfig.resolve(body)
+    stage('Unit Testing'){
+        withMaven(
+            maven: "${mavenName}"
+        ){
+            script{
+                try{
+                      mvn test
+                }catch (err){
+                    echo 'Error while perfomring Unit Testing of maven repo'
+                }
+            }
         }
     }
 }
